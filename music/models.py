c75e3134b1f091aec62ec54a818ce9ac91a8b0d4
Geovane Alves <0001157770@senaimgaluno.com.br>
2026-04-18 14:27:11 -0300

    feat: configura settings e cria modelos Categoria, Music e Playlist (criterio 1)

--- music/models.py
+++ music/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+
+class Music(models.Model):
+    titulo = models.CharField(max_length=200)
+    artista = models.CharField(max_length=200)
+    tempo = models.CharField(max_length=10, help_text="Ex: 3:45")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Playlist(models.Model):
+    nome = models.CharField(max_length=100)
+    musicas = models.ManyToManyField(Music)
+    
+    def __str__(self):
+        return self.nome
