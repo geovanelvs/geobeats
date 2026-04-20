2a453281e2c0c37fb8a1b6e4c95b94907d8efb04
Geovane Alves <0001157770@senaimgaluno.com.br>
2026-04-20 09:37:45 -0300

    UI: Adiciona CSS global, toasts de mensagens e corrige configurações do settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'music',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -117,5 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',]
 LOGIN_REDIRECT_URL = 'music_list'
 LOGOUT_REDIRECT_URL = 'login'
